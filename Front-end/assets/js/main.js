9c88b9df02258818abc7d9928b55fa23ba60a0b1
Brajo24 <sikandamartin77@gmail.com>
2025-11-17 15:40:09 +0200

    made changes to my main.js about the sidebar menu that wasn't opening

--- Front-end/assets/js/main.js
+++ Front-end/assets/js/main.js
@@ -3,6 +3,9 @@ fetch("../components/navbar.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
+
+    // Now the navbar is loaded, so attach the sidebar toggle event
+    initSidebarToggle();
   });
 
 // Load Footer
@@ -12,14 +15,14 @@ fetch("../components/footer.html")
     document.getElementById("footer").innerHTML = data;
   });
 
-// Sidebar Menu Toggle
-document.addEventListener("click", () => {
+// Function to set up sidebar toggle AFTER navbar is loaded
+function initSidebarToggle() {
   const sidebar = document.getElementById("sidebar");
   const toggleBtn = document.getElementById("toggleBtn");
 
-  if (toggleBtn) {
+  if (toggleBtn && sidebar) {
     toggleBtn.addEventListener("click", () => {
       sidebar.classList.toggle("active");
     });
   }
-});
+}
